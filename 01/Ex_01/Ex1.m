785a88b3d3a9c691fa6f00bdd568156a540fd946
enlupi <115542144+enlupi@users.noreply.github.com>
2023-10-13 18:52:48 +0200

    Fixed typo

--- 01/Ex_01/Ex1.m
+++ 01/Ex_01/Ex1.m
@@ -3,7 +3,7 @@ close all;
 
 %Load the image 'eleph2.jpg'
 
-img = imread("eleph2.jpg");
+img = imread('eleph2.jpg');
 
 
 %Plot the original image
@@ -18,7 +18,7 @@ BW_img = double(rgb2gray(img));
 
 %Plot the Black and White image
 
-figure, imagesc(BW_img); colormap(gray); title("B&W Image")
+figure, imagesc(BW_img); colormap(gray); title('B&W Image')
 
 
 %Define the window dimension, first try with window size 3x3, after change
@@ -48,7 +48,7 @@ var = conv2(padded_img.^2, filter, 'valid') - ...
 
 %Plot resulting matrix which cointains local variance
 
-figure, imagesc(var); colormap(gray); title("Local Variance")
+figure, imagesc(var); colormap(gray); title('Local Variance')
 
 
 %Define a Threshold
@@ -62,5 +62,5 @@ var(var < t) = 0;
 
 %Plot detected edges
 
-figure, imagesc(var); colormap(gray); title("Detected Edges")
+figure, imagesc(var); colormap(gray); title('Detected Edges')
 
